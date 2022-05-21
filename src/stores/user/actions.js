85d0461d624ee2bf95f07955c56c71ccf4717a01
Miharbi Hernandez <miharbihernandez@gmail.com>
2022-05-21 10:08:06 -0400

    user Books as Object

--- src/stores/user/actions.js
+++ src/stores/user/actions.js
@@ -1,6 +1,6 @@
 import { firebaseDb } from '../../boot/firebase'
 import { Loading } from 'quasar'
-import { getDoc, doc } from "firebase/firestore";
+import { getDoc, doc, setDoc } from "firebase/firestore";
 import { authStore } from '../auth/index.js'
 
 export default {
@@ -12,16 +12,42 @@ export default {
       const docSnap = await getDoc(usersRef)
       if (docSnap.exists()) {
         let data = docSnap.data()
+        const books = new Map(Object.entries(data.books));
         this.user = {
           id: auth.user.uid,
           displayName: auth.user.displayName,
           email: auth.user.email,
           photoURL: auth.user.photoURL,
           phoneNumber: auth.user.phoneNumber,
-          activedBook: data.books.find(book => book.active).uid,
+          activedBook: [...books.values()].find(book => book.active).uid,
           ...data
         }
+      } else {
+        await this.createUser()
       }
+    }
+    Loading.hide()
+  },
+  async createUser () {
+    const auth = authStore()
+    if (auth.user?.uid) {
+      Loading.show()
+      let data = {
+        id: auth.user.uid,
+        displayName: auth.user.displayName,
+        email: auth.user.email,
+        photoURL: auth.user.photoURL,
+        phoneNumber: auth.user.phoneNumber,
+        books: {
+          [auth.user.uid]: {
+            uid: auth.user.uid,
+            active: true,
+          }
+        }
+      }
+      let usersRef = doc(firebaseDb, "users", auth.user.uid)
+      await setDoc(usersRef, data)
+      this.user = data
     }
     Loading.hide()
   }
