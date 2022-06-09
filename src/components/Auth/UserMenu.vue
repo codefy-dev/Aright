4d20e1a61abdabc27babeab742708f4c4d22ecb6
Miharbi Hernandez <miharbihernandez@gmail.com>
2022-06-09 18:14:45 -0400

    User menu + Book List Template

---
+++ src/components/Auth/UserMenu.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-avatar class="cursor-pointer" icon="account_circle">
+    <q-menu>
+      <div class="row no-wrap q-pa-sm">
+        <div class="column items-center">
+          <q-avatar size="72px">
+            <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+          </q-avatar>
+          <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
+          <q-btn
+            flat
+            label="Editar"
+            icon="edit"
+            size="sm"
+            v-close-popup
+            class="q-mt-md"
+            color="primary"
+          />
+        </div>
+        <q-separator vertical inset class="q-mx-md" />
+        <div class="column">
+          <div class="text-subtitle1 q-mb-md text-primary">Configuración</div>
+          <q-toggle size="sm" label="Modo Dark" />
+          <q-toggle size="sm" label="Bluetooth" />
+          <q-separator />
+          <q-btn
+            label="Logout"
+            icon="logout"
+            size="sm"
+            flat
+            v-close-popup
+            @click="auth.logout"
+            class="q-mt-md"
+            color="negative"
+          />
+        </div>
+      </div>
+    </q-menu>
+  </q-avatar>
+</template>
+
+<script>
+import { authStore } from 'stores/auth/'
+export default {
+  name: 'account-settings',
+  setup () {
+    const auth = authStore()
+    return {
+      auth
+    }
+  }
+}
+</script>
